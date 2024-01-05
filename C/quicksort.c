372e51f7c299b8d47eb1233bf7dc1b500eaa7bca
Rakesh <rakeshchoudhary11032004@gmail.com>
2024-01-05 12:18:49 +0530

    Quicksort

--- C/quicksort.c
+++ C/quicksort.c
@@ -1,55 +1,54 @@
-#include<stdio.h>
-#include<conio.h>
-void quicksort(int[10],int,int);
-int partition(int[10],int,int);
-void main(){
+#include <stdio.h>
 
-    int x[20],size,i;
+void quicksort(int[], int, int);
+int partition(int[], int, int);
 
-    printf("Enter siz of an array:");
-    scanf("%d",&size);
+int main() {
+    int x[20], size, i;
 
-    printf("Enter %d elements:",size);
-    for(i=0;i<size;i++){
+    printf("Enter size of an array:");
+    scanf("%d", &size);
 
-        scanf("%d",&x[i]);
+    printf("Enter %d elements:\n", size);
+    for (i = 0; i < size; i++) {
+        scanf("%d", &x[i]);
     }
 
-    quicksort(x,0,size-1);
+    quicksort(x, 0, size - 1);
 
     printf("Sorted Elements:");
-    for(i=0;i<size;i++){
-        printf("%d",x[i]);
-
+    for (i = 0; i < size; i++) {
+        printf("%d ", x[i]);
     }
-  getch();
+
+    return 0;
 }
 
-void quicksort(int x[10],int first,int last){
-
+void quicksort(int x[], int first, int last) {
     int mid;
-    if(first < last){
-        mid=partition(int x,int first,int last);
-        quicksort(x,first,mid-1);
-        quicksort(x,mid+1,last);
-    }
 
+    if (first < last) {
+        mid = partition(x, first, last);
+        quicksort(x, first, mid - 1);
+        quicksort(x, mid + 1, last);
+    }
 }
-int partition(int x[10],int p,int r){
-    int value,i,j,temp;
-    value=x[r];
-    i=p-1;
-    for(j=p;j<=r-1;j++){
-        if(x[j]<=value){
-            i=i+1;
-            temp=x[i];
-            x[i]=x[j];
-            x[j]=temp;
+
+int partition(int x[], int p, int r) {
+    int value, i, j, temp;
+    value = x[r];
+    i = p - 1;
+    for (j = p; j <= r - 1; j++) {
+        if (x[j] <= value) {
+            i = i + 1;
+            temp = x[i];
+            x[i] = x[j];
+            x[j] = temp;
         }
     }
-    temp=x[i+1];
-    x[i]=x[r];
-    x[r]=temp;
+    temp = x[i + 1];
+    x[i + 1] = x[r];
+    x[r] = temp;
 
-    return(i+1);
+    return (i + 1);
 }
